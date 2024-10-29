98df744ae8e2b85b9a55aebb875bd04baeedc8fa
anik <ahmmed.anik75@gmail.com>
2024-10-29 14:12:40 +0600

    update

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<x-app-layout>
+
+<meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+
+<h1 class="text-3xl font-bold text-center">Welcome to My Dashboard</h1>
+    
+</body>
+
+</x-app-layout>
+</html>
+
